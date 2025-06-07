90797a3bc023c044a24db00057f8db8c1563caa8
3DPrintTech <3dprinttech@seznam.cz>
2025-06-07 18:32:08 +0200

    Add files via upload

---
+++ libs/libs/OrbitControls.js
@@ -0,0 +1,2 @@
+// Placeholder pro OrbitControls.js – správně importovatelné jako ES modul
+export { OrbitControls } from 'https://unpkg.com/three@0.150.1/examples/jsm/controls/OrbitControls.js';
---
+++ libs/libs/three.module.js
@@ -0,0 +1,2 @@
+// Placeholder for three.module.js – ve skutečném projektu by měl být celý soubor
+export * from 'https://unpkg.com/three@0.150.1/build/three.module.js';
