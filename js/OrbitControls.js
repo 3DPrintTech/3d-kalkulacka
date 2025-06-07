537bed8476570cb7898442204a9032d7afdec8d0
3DPrintTech <3dprinttech@seznam.cz>
2025-06-07 17:53:36 +0200

    Add files via upload

---
+++ js/OrbitControls.js
@@ -0,0 +1,2 @@
+// OrbitControls modul - placeholder pro OrbitControls.js
+export * from 'https://unpkg.com/three@0.150.1/examples/jsm/controls/OrbitControls.js';
---
+++ js/STLLoader.js
@@ -0,0 +1,2 @@
+// STLLoader modul - placeholder pro STLLoader.js
+export * from 'https://unpkg.com/three@0.150.1/examples/jsm/loaders/STLLoader.js';
