53604f1a2c92af685a60df0e1999f74deee8fad1
3DPrintTech <3dprinttech@seznam.cz>
2025-06-12 22:50:17 +0200

    Delete libs directory

--- libs/OrbitControls.js
+++
@@ -1,2 +0,0 @@
-// Placeholder pro OrbitControls.js – správně importovatelné jako ES modul
-export { OrbitControls } from 'https://unpkg.com/three@0.150.1/examples/jsm/controls/OrbitControls.js';
--- libs/STLLoader.js
+++
@@ -1,2 +0,0 @@
-// Placeholder pro STLLoader.js – správně importovatelné jako ES modul
-export { STLLoader } from 'https://unpkg.com/three@0.150.1/examples/jsm/loaders/STLLoader.js';
